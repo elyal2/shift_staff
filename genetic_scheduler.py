c99539ccdea0d2ece57c6569d6c1a0c8b830d6b4
elyal2 <fcoalberto@gmail.com>
2023-09-18 15:15:29 +0200

    supress warnings

--- genetic_scheduler.py
+++ genetic_scheduler.py
@@ -1,31 +1,46 @@
 import numpy
 import random
 from deap import base, creator, tools, algorithms
+from functools import partial
 
+# DEAP uses a dynamic approach to add methods to these objects, which can confuse static type checkers. 
+# However, this doesn't mean that your code is incorrect. 
+# If code runs without any runtime errors, we can safely ignore these warnings.
 class GeneticScheduler:
     def __init__(self, num_employees, num_jobs):
         self.num_employees = num_employees
         self.num_jobs = num_jobs
 
         creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-        creator.create("Individual", list, fitness=creator.FitnessMin)
+        creator.create("Individual", list, fitness=creator.FitnessMin) # type: ignore
 
         self.toolbox = base.Toolbox()
-        self.toolbox.register("attr_int", random.randint, 0, num_employees-1)
-        self.toolbox.register("individual", tools.initRepeat, creator.Individual, self.toolbox.attr_int, num_jobs)
-        self.toolbox.register("population", tools.initRepeat, list, self.toolbox.individual)
+        self.toolbox.register("individual", self.init_individual)  
+        self.toolbox.register("population", self.init_population)  # type: ignore
 
         self.toolbox.register("evaluate", self.cost)
         self.toolbox.register("mate", tools.cxTwoPoint)
-        self.toolbox.register("mutate", tools.mutUniformInt, low=0, up=num_employees-1, indpb=0.1)
+        self.toolbox.register("mutate", self.mutate)
         self.toolbox.register("select", tools.selTournament, tournsize=3)
+
+    def attr_int(self):
+        return random.randint(0, self.num_employees-1)
+
+    def init_individual(self):
+        return creator.Individual([self.attr_int() for _ in range(self.num_jobs)]) # type: ignore
+
+    def init_population(self, n):
+        return [self.toolbox.individual() for _ in range(n)] # type: ignore
+
+    def mutate(self, individual):
+        return tools.mutUniformInt(individual, low=0, up=self.num_employees-1, indpb=0.1)
 
     def cost(self, individual):
         # This is a placeholder. You should implement this method based on your specific cost function.
         return sum(individual),
 
     def run(self, ngen=50, pop_size=100):
-        pop = self.toolbox.population(n=pop_size)
+        pop = self.toolbox.population(n=pop_size) # type: ignore
         hof = tools.HallOfFame(1)
         stats = tools.Statistics(lambda ind: ind.fitness.values)
         stats.register("avg", numpy.mean)
